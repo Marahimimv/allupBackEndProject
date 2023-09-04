509ae7d6fa562f823ac451f573fe89582ef1b491
Marahimimv <marahimii@code.edu.az>
2023-09-04 13:36:01 +0400

    first

--- allupBackEndProject/DataAccessLayer/AppDbContext.cs
+++ allupBackEndProject/DataAccessLayer/AppDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using allupBackEndProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace allupBackEndProject.DataAccessLayer
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Setting> Settings{ get; set; } 
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ allupBackEndProject/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace allupBackEndProject.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string? Image { get; set; }
+        public bool IsMain { get; set; }
+        public int? ParentId { get; set; }
+        public Category? Parent { get; set; }
+
+    }
+}
